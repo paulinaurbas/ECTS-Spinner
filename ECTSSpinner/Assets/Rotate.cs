6fce37c57b129b760096540e33c7d5b0389f25b2
Michał Herjan <michal@herjan.pl>
2020-04-01 20:57:05 +0200

    Pierwsze kroki
    - Dodano kulkę
    - Dodano cylinder po którym porusza się kulka
    - Dodano skrypt ruchu i obrotu dla kulki oraz kamerę podążającą za kulka

---
+++ ECTSSpinner/Assets/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class Rotate : MonoBehaviour
+{
+    public float rotateSpeed = 1.0f;
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        transform.Rotate(rotateSpeed, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+        controller.SimpleMove(new Vector3(0.0f, 0.0f, 0.0f));
+    }
+}
---
+++ ECTSSpinner/Assets/SimpleMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))]
+public class SimpleMove : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+    public float horizontalSpeed = 1.0f;
+    public float verticalSpeed = 5.0f;
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        float horizontal = Input.GetAxis("Horizontal") * horizontalSpeed;
+        float vertical = verticalSpeed;
+
+        transform.Rotate(new Vector3(vertical, 0, horizontal));
+        controller.SimpleMove(new Vector3(horizontal, 0.0f, vertical));
+    }
+}
