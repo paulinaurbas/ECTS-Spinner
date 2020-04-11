b4ea8afc970fdb9b52fae45f7933ff6ec7a3ceb5
Paulina Urbaś (paulurb875) <paulurb875@student.polsl.pl>
2020-04-11 17:39:11 +0200

    Ball movement by accelometer
    - add script to drive ball by accelometer
    - changed project properties only for landscape

---
+++ ECTSSpinner/Assets/Scripts/Accelometer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Accelometer : MonoBehaviour
+{
+ 
+    private Rigidbody rb;
+    private int ballSpeed = 2;
+    private float forwardSpeed = 20.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+    void AccelometrMove()
+    {
+        float x = Input.acceleration.x;
+        Debug.Log("X = " + x);
+
+        if (x < -0.1f)
+        {
+            MoveLeft();
+        }
+        else if (x > 0.1f)
+        {
+            MoveRight();
+        }
+        else
+        {
+            SetVelocityZero();
+        }
+    }
+
+    private void SetVelocityZero()
+    {
+        rb.velocity = new Vector3(0, 0, forwardSpeed);
+    }
+
+    private void MoveRight()
+    {
+        rb.velocity = new Vector3(ballSpeed,0, forwardSpeed);
+    }
+
+    private void MoveLeft()
+    {
+        rb.velocity = new Vector3(-ballSpeed, 0, forwardSpeed);
+    }
+
+    void Update()
+    {
+        AccelometrMove();
+    }
+
+
+}
+
---
+++ MojaNauka/Assets/Scripts/PlayerConntroller.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerConntroller : MonoBehaviour
+{
+   
+        private Rigidbody rb;
+        private int ballSpeed = 10;
+        private float forwardSpeed = 5.0f;
+
+        void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+    
+        }
+        void AccelometrMove()
+        {
+            float x = Input.acceleration.x;
+            Debug.Log("X = " + x);
+            
+            if(x < -0.1f)
+            {
+                MoveLeft();
+            }
+            else if (x > 0.1f)
+            {
+                MoveRight();
+            }
+            else
+            {
+                SetVelocityZero();
+            }
+        }
+
+    private void SetVelocityZero()
+    {
+        rb.velocity = Vector3.zero;
+    }
+
+    private void MoveRight()
+    {
+        rb.velocity = new Vector3(ballSpeed, 0, forwardSpeed); 
+    }
+
+    private void MoveLeft()
+    {
+        rb.velocity = new Vector3(-ballSpeed, 0, forwardSpeed);
+    }
+
+    void Update()
+        {
+        AccelometrMove();
+    }
+
+
+}
+
