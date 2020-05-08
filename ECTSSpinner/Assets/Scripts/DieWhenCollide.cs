5d0c535892011035aa8989b521539682467356ed
franpog859 <franciszek.pog@gmail.com>
2020-05-08 19:24:01 +0200

    Add finish of the level and the obstacle

---
+++ ECTSSpinner/Assets/Scripts/DieWhenCollide.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieWhenCollide: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ball")
+        {
+            Destroy(collision.gameObject);
+            // TODO: Add dying fireworks
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ECTSSpinner/Assets/Scripts/FinishLevel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ball")
+        {
+            Destroy(collision.gameObject);
+            // TODO: Do not destroy the ball, move it to the start position
+            //       Regenerate the level, change colors and things like that
+            //       OR just change the scene!
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
