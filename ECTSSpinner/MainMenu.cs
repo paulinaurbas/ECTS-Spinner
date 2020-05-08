d32782207a2f5b5222a44f49f8c72d8a45dbc01d
Paulina Urbaś (paulurb875) <paulurb875@student.polsl.pl>
2020-05-08 20:36:29 +0200

    Menu added

---
+++ ECTSSpinner/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void PlayGame()
+    {
+        
+    }
+
+}
