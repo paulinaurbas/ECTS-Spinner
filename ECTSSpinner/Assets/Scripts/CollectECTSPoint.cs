8b951904e3b9e4c3c67bbb0a78a0a8a39b96a943
Franciszek Pogodziński <32485355+franpog859@users.noreply.github.com>
2020-05-08 19:53:35 +0200

    Add ECTS point, script for the obstacle and the finish plane (#3)

---
+++ ECTSSpinner/Assets/Scripts/CollectECTSPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectECTSPoint : MonoBehaviour
+{
+
+    public int ECTSPoints;
+    public Text ECTSPointsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "ECTSPoint")
+        {
+            Destroy(collision.gameObject);
+            // TODO: Add fireworks effects 
+            ECTSPoints++;
+            ECTSPointsText.text = "POINTS: " + ECTSPoints.ToString();
+            Debug.Log("ECTSPoints = " + ECTSPoints);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ECTSSpinner/Assets/Scripts/DieWhenCollide.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieWhenCollide: MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Ball")
+        {
+            // TODO: Add dying fireworks
+            // TODO: Transfer the scene to some MENU without the ball 
+            //       and the accelerometr script. It collapses in this 
+            //       version due to the fact that it tries to change 
+            //       position of the ball even if it is destroyed
+            Destroy(collision.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
